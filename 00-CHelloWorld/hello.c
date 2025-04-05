ade949e7f1eca9d485c44144b8a3df56ec20c949
Pablo Rodriguez <pablrodriguez@frba.utn.edu.ar>
2025-04-05 14:13:21 -0300

    Agrego TP0

---
+++ 00-CHelloWorld/hello.c
@@ -0,0 +1,12 @@
+/* Qué: Hello World
+ * Breve descripción: Programa que imprime "Hello, World!" en la salida estándar.
+ * Quiénes: Pablo Rodriguez
+ * Cuándo: 2025-04-05
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, World!\n");
+}
---
+++ 00-CHelloWorld/readme.md
@@ -0,0 +1,3 @@
+Compilador: Clang
+Versión de Clang: 17.0.0 (clang-1700.0.13.3)
+Versión de C: C23 (202311)
